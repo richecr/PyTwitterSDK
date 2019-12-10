e28283424a268b01a53f83e47dfb9eef3e439454
Rich <richelton14@gmail.com>
2019-12-10 09:41:47 -0300

    Substituição da forma de requisitar a API Concluída

--- exec_bibli_twitter.py
+++ exec_bibli_twitter.py
@@ -2,25 +2,10 @@ from py_twitter import PyTwitter
 import json, requests
 from requests_oauthlib import OAuth1
 
-consumer_Key = 'rrUDLNZVqv8DaWX6fkmNrB5V9'
-consumer_Secret = 'R20GkXiu42758yyy5pfykcswYA7Lnn9rBjhQEN25jMCPYO1YS7'
+consumer_Key = 'XXX'
+consumer_Secret = 'XXX'
 
-token_Key = '2455702491-8jbRT6j6tLv5JHkL7WAac31ZfAAcluFRSDsWXXK'
-token_Secret = 'AuaU4YduVSXtcN1oxrbpYs0E3p3AES0xekg6lCzXEtEHW'
-
+token_Key = 'XXX'
+token_Secret = 'XXX'
 
 twitter = PyTwitter(consumer_Key, consumer_Secret, token_Key, token_Secret)
-
-# print(twitter.search("flamengo", "pt-br", "extended")[0])
-
-# publicar = twitter.novoTweet('Olá Mundo')
-# print(publicar)
-
-t = twitter.geo("campina grande")
-
-print(t[10])
-# pesquisa = twitter.search('São Paulo', 'pt')
-
-# for resultado in pesquisa:
-#     print(resultado['text'])
-#     print(resultado['user']['screen_name'])
--- py_twitter/PyTwitter.py
+++ py_twitter/PyTwitter.py
@@ -1,62 +1,65 @@
 import urllib.parse
-import json, requests
+import json
+import requests
 from requests_oauthlib import OAuth1
+
 
 class PyTwitter:
     def __init__(self, consumer_key, consumer_secret, token_key, token_secret):
-        self.conexao(consumer_key, consumer_secret, token_key, token_secret)
+        self.base_uri = "https://api.twitter.com/1.1"
+        self.base_uri_stream = "https://stream.twitter.com/1.1"
+        self.auth = self.conexao(consumer_key, consumer_secret, token_key, token_secret)
 
     def conexao(self, consumer_key, consumer_secret, token_key, token_secret):
-        self.auth = OAuth1(consumer_key, consumer_secret, token_key, token_secret)
+        return OAuth1(consumer_key, consumer_secret, token_key, token_secret)
 
     def novoTweet(self, novo_tweet):
-        query_codificada = urllib.parse.quote(novo_tweet, safe='')  # Para pesquisar com sinais(ex: #, acentos etc).
-        response = requests.post('https://api.twitter.com/1.1/statuses/update.json?status=' + query_codificada, auth=self.auth)
+        query_codificada = urllib.parse.quote(novo_tweet, safe='')
+        uri = self.base_uri + '/statuses/update.json?status=' + query_codificada
+        response = requests.post(uri, auth=self.auth)
         response = response.json()
         return response
 
     def search(self, query, lang="pt-br", tweet_mode=""):
-        query_codificada = urllib.parse.quote(query, safe='')  # Para pesquisar com sinais(ex: #, acentos etc).
-        response = requests.get('https://api.twitter.com/1.1/search/tweets.json?q=' + query_codificada + '&tweet_mode=' + tweet_mode + '&lang=' + lang, auth=self.auth)
+        query_codificada = urllib.parse.quote(query, safe='')
+        uri = self.base_uri + '/search/tweets.json?q=' + query_codificada + '&tweet_mode=' + tweet_mode + '&lang=' + lang
+        response = requests.get(uri, auth=self.auth)
         response = response.json()
         tweetes = response['statuses']
         return tweetes
 
     def geo(self, query):
         query_codificada = urllib.parse.quote(query, safe='')
-        response = requests.get('https://api.twitter.com/1.1/geo/search.json?query=' + query_codificada, auth=self.auth)
+        uri = self.base_uri + '/geo/search.json?query=' + query_codificada
+        response = requests.get(uri, auth=self.auth)
         response = response.json()
         tweets = response['result']['places']
         return tweets
 
     def show(self, query):
-        uri = 'https://api.twitter.com/1.1/statuses/show.json?id=%s&tweet_mode=extended' % query
-        requisicao = self.cliente.request(uri)
-        decodificar = requisicao[1].decode()
-        tweets = json.loads(decodificar)
-        return tweets
+        uri = self.base_uri + '/statuses/show.json?id=%s&tweet_mode=extended' % query
+        response = requests.get(uri, auth=self.auth)
+        tweet = response.json()
+        return tweet
     
     def show_lookup(self, ids):
         tweets = []
         for id in ids:
-            uri = 'https://api.twitter.com/1.1/statuses/show.json?id=%s&tweet_mode=extended' % id
-            requisicao = self.cliente.request(uri)
-            decodificar = requisicao[1].decode()
-            tweets = json.loads(decodificar)
-            tweets.append(tweets)
+            tweet = self.show(id)
+            tweets.append(tweet)
 
         return tweets
 
     def retweetar(self, query):
-        url = "https://api.twitter.com/1.1/statuses/retweet/"+str(query) + ".json"
-        requisicao = self.cliente.request(url, method='POST')
-        decodificar = requisicao[1].decode()
-        objeto = json.loads(decodificar)
-        return objeto
+        uri = self.base_uri + "/statuses/retweet/"+str(query) + ".json"
+        response = requests.post(uri, auth=self.auth)
+        tweet = response.json()
+        return tweet
     
     def filter(self, query):
-        query_codificada = urllib.parse.quote(name, safe='')
-        response = requests.post('https://stream.twitter.com/1.1/statuses/filter.json?locations=' + query_codificada, auth=self.auth, stream=True)
+        query_codificada = urllib.parse.quote(query, safe='')
+        uri = self.base_uri_stream + '/statuses/filter.json?track=' + query_codificada
+        response = requests.post(uri, auth=self.auth, stream=True)
         print(response)
         response = response.json()
         tweets = response['result']['places']
